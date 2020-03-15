e8de705429ed2d86734bd395baa1de49ca4ce31e
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-03-15 23:36:15 +0100

    add  function Math.ceil()

---
+++ century.js
@@ -0,0 +1,14 @@
+//8 kyu Century from Year
+
+//The first century spans from the year 1 up to and including the year 100, 
+//The second - from the year 101 up to and including the year 200, etc.
+
+//Task :
+//Given a year, return the century it is in.
+
+//Math.ceil() - function always rounds a number up to the next largest whole number or integer.
+
+function century (year) {
+    return Math.ceil(year /100);
+    }
+
